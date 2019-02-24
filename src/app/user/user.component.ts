0540f65541887ea9b7aec07fbb40ea7270b5130f
Azeem Syed <azeempsyed@gmail.com>
2019-02-24 19:26:32 +0530

    user component updates

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login/login.service';
 
 @Component({
@@ -7,41 +6,19 @@ import { LoginService } from '../login/login.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  numbers = [];
-  showFiller = false;
-  
+export class UserComponent implements OnInit {  
   constructor(
-    changeDetectorRef: ChangeDetectorRef, 
-    media: MediaMatcher,
     public loginService: LoginService
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+  ) {  }
 
   ngOnInit() {
-    let i = 0;
-    for(;i<20;i++) {
-      this.numbers.push(`Example text number: ${i}`);
-    }
+
   }
-  mobileQuery: MediaQueryList;
-
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from({length: 50}, () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
-  private _mobileQueryListener: () => void;
-  
+  checkbutton() {
+    console.log("bunnies")
+  }
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+
   }
 
 }
